81d582357dfd19ec09fbe468934c3c163cb66db4
Csandrade1 <andrade.cynthia07@gmail.com>
2022-04-22 16:57:03 -0700

    almost done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Character from "./components/Character";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character data={data} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,38 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const CharactersDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    width: 60%;
+    margin: 0 auto;
+    font-size: 25px;
+    padding: 0.5rem;
+    border: 2px black solid;
+    margin-top: 2%;
+  }
+
+  div h2 h2 {
+    font-size: 10px;
+  }
+`;
+
+export default function Character(props) {
+  return (
+    <CharactersDiv>
+      {props.data.map((obj, idx) => {
+        return (
+          <div key={idx}>
+            <h2>{obj.name}</h2>
+            <h2>{obj.birth_year}</h2>
+          </div>
+        );
+      })}
+    </CharactersDiv>
+  );
+}
